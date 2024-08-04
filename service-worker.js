7dede2c8e2dd39546877fef69534415c98d029d5
MayThillays <maybelline.thillays@gmail.com>
2024-08-04 00:26:57 -0400

    modif

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v3';
 
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     'index.html'
 ];
 
-//INSTALLATION
+//INSTALLATION du serice-worker
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
     // Precache static resources here. MISE EN CACHE
@@ -20,16 +20,43 @@ self.addEventListener('install', (evt) => {
     self.skipWaiting();
 });
 
-//ACTIVATION
+
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
-    //Remove previous cached data from disk.
+
+
+    //Suppression de la vieille cache si le nom est différent
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache',
+                        key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
     self.clients.claim();
 });
 
 
+
 //Accès a une ressource
-self.addEventListener('activate', (evt) => {
-    console.log('[ServiceWorker] Fetch' , evt.request.url);
-    //add fetch event handler here
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('/Cochenille/PointNClick/offline . h t m l ');
+                    });
+            })
+    );
 });
